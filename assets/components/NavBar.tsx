be84b341a923e1e66335628abfcdc9debc7cfd93
stephMerlu <stephanie.merluzzo@gmail.com>
2023-08-08 14:52:06 +0200

    Création composant NavBar

---
+++ assets/components/NavBar.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Logo from '../components/Logo';
+
+
+function NavBar() {
+  return (<>
+    <Navbar collapseOnSelect expand="lg" className="bg-secondary navbar-dark">
+      <Container>
+        <Logo className='bg-white rounded-circle m-2 p-2 NavBarLogo'></Logo>
+        <Navbar.Brand href="#home">Oktopuce</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#information">Information</Nav.Link>
+            <Nav.Link href="#historique">Historique</Nav.Link>
+            <Nav.Link href="#intervention">Intervention</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#connection">Connexion</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </>
+  );
+}
+
+export default NavBar;
