97dceb0877df75bc2dbaae0015661dbe90978ac6
melodieot <melodie.olory@gmail.com>
2023-08-08 09:30:34 +0200

    Ajout variables couleurs

--- assets/components/Button.tsx
+++ assets/components/Button.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Button({text}) {
     return (
-        <button>{text}</button>
+        <button style="text-transform: uppercase;">{text}</button>
     );
 }
