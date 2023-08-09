63407316d125098554cef791fd2aacf082a3cd6f
Nesami Haiiro <20476281+citrusMarmelade@users.noreply.github.com>
2023-08-09 11:59:31 +0200

    Correction page acceuil et boutons Ajout global UDM React

--- assets/pages/Home.tsx
+++ assets/pages/Home.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 /** @ts-ignore */
 import Button from '../components/Button';
+// import Button from 'react-bootstrap';
 import Logo from '../components/Logo';
 
 export default function Home(){
-    return (
-    <>
+    return (<>
         <section className='fullHome d-flex flex-column align-items-center p-3'>
             <h1 className='homeTitle text-uppercase'>Oktopuce</h1>
             <Logo className="mt-5 p-5 logoHome"></Logo>
-            <div className="btnDiv m-auto d-flex flex-column">
-                <Button className='text-uppercase btnHome mb-3' variant="primary" text="J'ai un compte"/>
-                <Button className='text-uppercase btnHome' variant="primary" text="Je n'ai pas de compte"/>
+            <div className="btnDiv my-auto d-flex flex-column">
+                <Button className='text-uppercase btnHome mb-3' variant="primary">J'ai un compte</Button>
+                <Button className='text-uppercase btnHome' variant="primary">Je n'ai pas de compte"</Button>
             </div>
         </section>
-    </>
-    );
+    </>);
 }
