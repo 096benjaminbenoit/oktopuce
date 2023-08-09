6ca6c8125f72b233a466f03d6762a1131d4252f5
096benjaminbenoit <096benjaminbenoit@gmail.com>
2023-08-09 15:52:49 +0200

    easy admin gas fixed

--- src/Controller/Admin/GasTypesCrudController.php
+++ src/Controller/Admin/GasTypesCrudController.php
@@ -4,7 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\GasTypes;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 
 class GasTypesCrudController extends AbstractCrudController
 {
@@ -20,14 +22,11 @@ class GasTypesCrudController extends AbstractCrudController
             ->setEntityLabelInPlural('Gaz');
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name')->setLabel('Nom du gaz'),
+            NumberField::new('eq_co2_per_kg')->setLabel('Equivalent CO2 par kg'),
         ];
     }
-    */
 }
