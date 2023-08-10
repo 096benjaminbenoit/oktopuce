9b808d83fbbcedb8f0321fa8ec4a440b6186ceba
melodieot <melodie.olory@gmail.com>
2023-08-10 15:38:03 +0200

    Changement font du projet

--- assets/pages/Error404.tsx
+++ assets/pages/Error404.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 /** @ts-ignore */
 import Illustration from '../components/Illustration';
+import NavBar from "../components/NavBar";
 
 export default function scanPage(){
     return (
     <>
         {/* AJOUTER LA NAVBAR */}
-        {/* <NavBar></NavBar> */}
+        <NavBar></NavBar>
         <section className='error404 d-flex flex-column align-items-center p-3'>
-            <h1 className='homeTitle text-uppercase m-3'>Oktopuce</h1>
             <Illustration className='illustration404 img-fluid m-5'/>
             <h2 className="title404 text-uppercase">Erreur 404</h2>
             <p className="quote404 m-3">Oups ! Il semblerait que cette page ne soit pas disponible, revenez plus tard.</p>
