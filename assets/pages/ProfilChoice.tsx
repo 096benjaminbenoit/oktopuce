418252aac435f2bb5c550f7fff2f0c29aaec91a3
096benjaminbenoit <096benjaminbenoit@gmail.com>
2023-08-29 14:50:14 +0200

    profile & scan context ok

--- assets/pages/ProfilChoice.tsx
+++ assets/pages/ProfilChoice.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
 import Logo from '../components/Logo'
 import Button from '../components/Button'
+import { ProfileAndScanDispatchContext } from '../context/ProfileAndScanContext';
 
 export default function ProfilChoice() {
+    
+    const dispatch = useContext(ProfileAndScanDispatchContext);
+
   return (
     <section className='fullHome d-flex flex-column align-items-center p-5'>
             <div className='p-3'>
@@ -12,8 +16,8 @@ export default function ProfilChoice() {
                 <Logo className="mt-5 logoHome"></Logo>
             </div>
             <div className="btnDiv my-auto d-flex flex-column">
-                <Button.Link path='/scan' className='text-uppercase btnHome mb-3' variant="primary">Je suis un particulier</Button.Link>
-                <Button.Link path="/scan" className='text-uppercase btnHome' variant="primary">Je suis un professionnel</Button.Link>
+                <Button.Link path='/scan' className='text-uppercase btnHome mb-3' variant="primary" onClick={() => dispatch({type: "profileSelected", selectedProfile: "unprofessional"})}>Je suis un particulier</Button.Link>
+                <Button.Link path="/scan" className='text-uppercase btnHome' variant="primary" onClick={() => dispatch({type: "profileSelected", selectedProfile: "professionnal"})}>Je suis un professionnel</Button.Link>
             </div>
         </section>
   )
--- assets/react_app.tsx
+++ assets/react_app.tsx
@@ -11,6 +11,7 @@ import './styles/app.scss';
 
 import React, { useReducer } from 'react';
 import { LoginContext, LoginDispatchContext } from './context/LoginContext';
+import { ProfileAndScanContext, ProfileAndScanDispatchContext } from './context/ProfileAndScanContext';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import {
@@ -87,14 +88,19 @@ const queryClient = new QueryClient()
 
 function App() {
   const [login, loginDispatch] = useReducer(loginReducer, { loggedIn: false });
+  const [profileAndScan, profileAndScanDispatch] = useReducer(profileAndScanReducer, {});
 
   return (
     <QueryClientProvider client={queryClient}>
-      <LoginContext.Provider value={login}>
-        <LoginDispatchContext.Provider value={loginDispatch}>
-          <RouterProvider router={router} />
-        </LoginDispatchContext.Provider>
-      </LoginContext.Provider>
+      <ProfileAndScanContext.Provider value={profileAndScan}>
+        <ProfileAndScanDispatchContext.Provider value={profileAndScanDispatch}>
+          <LoginContext.Provider value={login}>
+            <LoginDispatchContext.Provider value={loginDispatch}>
+              <RouterProvider router={router} />
+            </LoginDispatchContext.Provider>
+          </LoginContext.Provider>
+        </ProfileAndScanDispatchContext.Provider>
+      </ProfileAndScanContext.Provider>
     </QueryClientProvider>
   );
 }
@@ -114,5 +120,20 @@ function loginReducer(login, action) {
     default: {
       throw Error('Unknown action: ' + action.type);
     }
+  }
+}
+
+function profileAndScanReducer(profileAndScan, action) {
+  switch (action.type) {
+    case 'profileSelected':
+      return {
+        ...profileAndScan,
+        profileType: action.selectedProfile,
+      }
+    case 'scan':
+      return {
+        ...profileAndScan,
+        nfcChip: action.nfcUUID
+      }
   }
 }
