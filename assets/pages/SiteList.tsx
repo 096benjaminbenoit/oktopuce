6bdba11d3133fa6d7b20830752e5e2a730d75aaf
melodieot <melodie.olory@gmail.com>
2023-08-30 10:45:32 +0200

    Requêtes page SiteList avec aide de EquipementList

--- assets/pages/SiteList.tsx
+++ assets/pages/SiteList.tsx
@@ -5,8 +5,10 @@ import { useForm } from 'react-hook-form';
 import { useQuery } from '@tanstack/react-query';
 import Site from './Site';
 import { Spinner } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 type Site = {
+  siteType: any;
   "id": number,
   "address": string,
   "city": string,
@@ -37,13 +39,27 @@ export default function SiteList() {
       
       <h1 className='titleInfosUser text-uppercase m-2'>Sites enregistrés</h1>
       
+      <div className="container mt-4">
+        <div className="card mx-auto" style={{ maxWidth: "500px" }}>
+          <div className="card-body">
+            {sites.map(site => (
+              <div key={site['@id']} className="site-item">
+                <p>{site.siteType.type}</p>
+              </div>
+            ))}
+            <div className="container text-center mt-3">
+              <Button.Link path='/site' className='text-uppercase btnInfosUser m-5' variant="primary"> Ajouter un site </Button.Link>
+            </div>
+            <Link to={''}></Link>
+          </div>
+        </div>
+      </div>
       
       {/* <ul className="siteList m-5">
         <li className='site site1'>Maison Perso</li> 
         <li className='site site2'>Bureau</li>
       </ul> */}
-
-      <Button.Link path='/site' className='text-uppercase btnInfosUser m-5' variant="primary"> Ajouter un site </Button.Link>
+      {/* <Button.Link path='/site' className='text-uppercase btnInfosUser m-5' variant="primary"> Ajouter un site </Button.Link> */}
     </section>
     </>
   );
